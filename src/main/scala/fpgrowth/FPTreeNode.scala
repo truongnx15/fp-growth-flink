878567db1520a6a42674e23deca9038c04fd9bdd
Truong Nguyen <truongnx15@gmail.com>
2016-01-11 12:08:13 +0100

    Adds FPTreeNode.scala back

---
+++ src/main/scala/fpgrowth/FPTreeNode.scala
@@ -0,0 +1,22 @@
+package fpgrowth
+
+import scala.collection.mutable
+
+/**
+  * Created by Xuan Truong on 10-Jan-16.
+  */
+class FPTreeNode(var item: Item, var frequency: Long, var parent: FPTreeNode) {
+
+  //Store children of FPGrowth node so it could be searched quickly
+  var children = mutable.HashMap.empty[Item, FPTreeNode]
+
+  //Next to next node (used in the header table)
+  var nextNode: FPTreeNode = null
+
+  //check if the node is root node
+  def isRoot: Boolean = item == null
+
+  override def toString(): String = {
+    return (item + ":" + frequency)
+  }
+}
